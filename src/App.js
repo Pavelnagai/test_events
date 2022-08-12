3f266598b44d3d9d557a3811e1490f5f7d3fa99b
Pavelnagai <pavel.nagai@mail.ru>
2022-08-12 03:33:02 +0300

    import icon fonts

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import Form from "./components/CreateSchool/CreateSchool";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
+import "./fontello/css/fontello.css"
 
 function App() {
     return (
-        <div className="App">
+        <div className="app">
             <Header/>
             <Body/>
             <Form/>
